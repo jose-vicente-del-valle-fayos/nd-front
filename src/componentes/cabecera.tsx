7dc25c9ff775066562e1a8d5779e6f5f722eb6ae
José Vicente del Valle Fayos <musheres.dioxinas.0p@icloud.com>
2024-03-10 09:53:44 +0100

    arreglados componentes cabecera pie y menu

--- src/componentes/cabecera.tsx
+++ src/componentes/cabecera.tsx
@@ -1,14 +1,12 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 
-class Cabecera extends Component {
-    render(){
-        return (
-            <header>
-                <h6><Link to={"https://www.nuestrodiario.es"}><span className="mdi">anchor</span></Link></h6>
-            </header>
-        )
-    }
+const Cabecera = () => {
+    return (
+        <header>
+            <h6><Link to={"https://www.nuestrodiario.es"}><span className="mdi">anchor</span></Link></h6>
+        </header>
+    )
 }
 
 export default Cabecera;
--- src/componentes/pie.tsx
+++ src/componentes/pie.tsx
@@ -7,10 +7,10 @@ const Pie = () => {
         <footer>
             <p>
                 <strong>
-                    <Link to="#" className="sin-subrayar" onClick={enviarCorreo}>escríbeme</Link> · <Link to={"/acceso"} className="sin-subrayar">acceso</Link> · <Link to={"/aviso"} className="sin-subrayar">aviso</Link> · <Link to={"/privacidad"} className="sin-subrayar">privacidad</Link>
+                    <Link to="#" className="sin-subrayar" onClick={enviarCorreo}>escríbeme</Link> • <Link to={"/acceso"} className="sin-subrayar">acceso</Link> • <Link to={"/aviso"} className="sin-subrayar">aviso</Link> • <Link to={"/privacidad"} className="sin-subrayar">privacidad</Link>
                 </strong>
             </p>
-            <h1><strong>© desde 2017 · José Vicente del Valle Fayos</strong></h1>
+            <h1><strong>© desde 2017 • José Vicente del Valle Fayos</strong></h1>
         </footer>
     )
 }
