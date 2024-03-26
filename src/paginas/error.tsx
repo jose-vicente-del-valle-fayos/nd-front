16cca9d5f3f2bac65c5e9461b82e90aed0c6312a
José Vicente del Valle Fayos <musheres.dioxinas.0p@icloud.com>
2024-03-26 11:30:41 +0100

    mejorada pagina de error

--- src/paginas/error.tsx
+++ src/paginas/error.tsx
@@ -12,8 +12,10 @@ const Error = () => {
         <motion.main initial="initial" animate="in" exit="out" variants={variantesPagina}>
             <article>
                 <h2>Error 404</h2>
-                <p>Estás buscando una página que no existe o estás intentando acceder sin permiso. Si estás experimentando otro problema, puedes <Link to={"/escribeme"}>contactar conmigo</Link> o volver a la <Link to={"/"}>página de inicio</Link>.</p>
-                <p>Si te animas a <Link to={"/escribeme"}>escribirme</Link>, no olvides indicar todos los detalles de tu consulta.</p>
+                <p><strong>Estás buscando una página que no existe</strong>. También puede ser hayas encontrado —en algún buscador o en alguna red social— <strong>un enlace entrante que pertenezca a una versión anterior de Nuestro Diario</strong>, donde las entradas se identificaban de otra manera. No descartes tampoco que haya borrado el contenido que buscas.</p>
+                <p>Si estás decidido a encontrar ese contenido, quizá puedas acceder a él desde la sección de <Link to={"/archivo"}>archivo</Link>, donde se publica una lista con todas las entradas de Nuestro Diario.</p>
+                <p>Si estás experimentando otro problema, puedes <Link to={"/escribeme"}>escribirme un mensaje</Link>, indicando todos los detalles de tu consulta. O puedes volver a la <Link to={"/"}>página de inicio</Link> y leer otra cosa…</p>
+                <img src={require("../imagenes/error.jpg")} alt={"Pepe"} title={"Pepe"}/>
             </article>
             <Pie/>
         </motion.main>
